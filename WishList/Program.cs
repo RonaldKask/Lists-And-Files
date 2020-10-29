cb6c588ae7a06115bdc91a663593caf1455733ab
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:36:28 +0200

    weirdga

---
+++ WishList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wishlist = new List<string>();
+            //string filePath = @"C:\Users\opilane\data\wishlist.txt";
+
+
+            for (int i = 0;i < 3; i++)
+            {
+                Console.WriteLine("Enter something to your wishlist: ");
+                wishlist.Add(Console.ReadLine());
+            }
+
+            string filePath = @"C:\Users\opilane\data\wishlist.txt";
+            File.WriteAllLines(filePath, wishlist);
+
+        }
+    }
+}
